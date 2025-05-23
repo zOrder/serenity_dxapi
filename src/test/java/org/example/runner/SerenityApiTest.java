383b4e49bcf125f6b351ebcf8aaea7a8e348856a
Christoph Henzelmann <chenzelmann@gmail.com>
2025-05-23 18:49:57 +0200

    renamed TestRunner - stupid me

---
+++ src/test/java/org/example/runner/SerenityApiTest.java
@@ -0,0 +1,14 @@
+package org.example.runner;
+
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        features = "classpath:features",
+        glue = "org.example.definitions",
+        plugin = { "pretty" }
+)
+public class SerenityApiTest {
+}
